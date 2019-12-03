13530de3b6e6e93d81dbc45fb991a68526b42aaa
Luis Garreta <lgarreta@uao.edu.co>
2019-12-03 09:53:52 -0500

    r103: Run with filtered data (plink QC)

--- back/gwaspoly-to-plink-formats.R
+++ back/gwaspoly-to-plink-formats.R
@@ -2,7 +2,29 @@
 
 # Convert gwaspoly genotype format to plink transposed format (tped, tfam)
 
+library (parallel)
+
 options(stringsAsFactors = FALSE)
+
+#------------------------------------------------------------------------------
+## Format and write tassel phenotype
+#------------------------------------------------------------------------------
+writeTasselPheno <- function (gwaspolyPhenotypeFile) {
+	gwaspolyPhenotype = read.table (file=gwaspolyPhenotypeFile, header=T)
+	idNames = as.character (gwaspolyPhenotype [,1])
+	Taxa = gwaspolyPhenotype [,2]
+	BLIGHT = gwaspolyPhenotype [,3]
+	plinkPheno = cbind (Taxa, BLIGHT)
+
+	message (">>> Writing tassel phenotype...")
+	plinkPhenoFilename = paste0 (strsplit (gwaspolyPhenotypeFile, split="[.]")[[1]][1], "-tassel.tbl")
+	sink (plinkPhenoFilename)
+	cat ("<Phenotype>\n")
+	cat ("taxa\tdata\n")
+	write.table (file="", plinkPheno, col.names=T, row.names=F, quote=F, sep="\t")
+	sink()
+}
+
 #------------------------------------------------------------------------------
 ## Write and format tassel structure
 #------------------------------------------------------------------------------
@@ -27,25 +49,33 @@ writeTasselStruct <- function (structureFile) {
 createPlinkPhenotype <- function (gwaspolyPhenotypeFile) {
 	phenotype = read.csv (file=gwaspolyPhenotypeFile, header=T)
 	idNames = as.character (phenotype [,1])
+	# Remove "And_" prefix
 	Samples = sapply (strsplit (idNames, split="And_",fixed=T), function (x) x[[2]][1])
 	BLIGHT = phenotype [,2]
-	plinkPheno = cbind (FID=Samples,IID=Samples, BLIGHT= BLIGHT)
 
-	message (">>> Writing tassel phenotype...")
+	message (">>> Writing plink phenotype...")
+	plinkPheno = cbind (FID=0,IID=Samples, BLIGHT= BLIGHT)
 	outFilename = paste0 (strsplit (gwaspolyPhenotypeFile, split="[.]")[[1]][1], "-plink.tbl")
-	sink (outFilename)
-	write.table (file=, plinkPheno, col.names=T, row.names=F, quote=F, sep="\t")
+	write.table (file=outFilename, plinkPheno, col.names=T, row.names=F, quote=F, sep="\t")
+
+	# Normalized phenotype
+	message (">>> Writing normalized plink phenotype...")
+	MinMaxScaling <- function(x){return((x-min(x))/(max(x)-min(x)))}
+	BLIGHT_normalized = round (MinMaxScaling (BLIGHT), 8)
+	plinkPheno = cbind (FID=0,IID=Samples, BLIGHT= BLIGHT_normalized)
+	outFilename = paste0 (strsplit (gwaspolyPhenotypeFile, split="[.]")[[1]][1], "-plink-norm.tbl")
+	write.table (file=outFilename, plinkPheno, col.names=T, row.names=F, quote=F, sep="\t")
 }
 
 #----------------------------------------------------------
 #----------------------------------------------------------
 createFileMAP <- function (genotype, plinkFilename) {
-	snpsIds = gsub ("solcap_snp_", "", gwaspolyGenotype [,1])
-	chrm    = genotype [,2]+1
-	pos     = genotype [3]
-
-	genoMAP    = cbind (chr=chrm, iid=snpsIds, dist=0, pos=pos)
 	message (">>> Writing MAP files...")
+	markersIds <- gsub ("solcap_snp_", "", genotype [,1])
+	chrm    <- genotype [,2]+1
+	pos     <- genotype [3]
+
+	genoMAP    = cbind (chr=chrm, iid=markersIds, dist=0, pos=pos)
 	filenameMAP = paste0 (plinkFilename, "-plink.map")
 	write.table (file=filenameMAP, genoMAP, col.names=F, row.names=F, quote=F, sep="\t")
 }
@@ -53,16 +83,31 @@ createFileMAP <- function (genotype, plinkFilename) {
 #----------------------------------------------------------
 #----------------------------------------------------------
 createFilePED <- function (genotype, plinkFilename) {
-	#namesGeno  = c("fid", "iid", "pid", "mid", "sex", "phe", rep ("X", ncol (talleles)))
-	alleles    = genotype [,-c(1,2,3)]
-	talleles   = tabAlleles (alleles)
-	#talleles   = t (alleles)
-	samplesIds = gsub ("And_", "", colnames (alleles))
-	genoPED    = cbind (samplesIds, samplesIds, 0,0,0,0, talleles)
-
 	message (">>> Writing PED files...")
+	#namesGeno  = c("fid", "iid", "pid", "mid", "sex", "phe", rep ("X", ncol (talleles)))
+	alleles    <<- genotype [,-c(1,2,3)]
+
+	message (">>> Creating transposed genotype...")
+	markersIds <<- gsub ("solcap_snp_", "", genotype [,1] )
+	samplesIds <<- gsub ("And_", "", colnames (alleles))
+	allelesTransposed <<- t(alleles)
+	rownames (allelesTransposed) = samplesIds
+	colnames (allelesTransposed) = markersIds
+
+	filenamePEDTransp = paste0 (plinkFilename, "-transposed.tbl")
+	write.table (file =filenamePEDTransp, allelesTransposed, col.names=T, row.names=T, quote=F, sep="\t")
+
+	message (">>> Creating PED genotype...")
+	talleles   = tabAlleles (alleles)
+	message (">>> ...Created PED genotype")
+	#talleles   = t (alleles)
+
+	colnames (talleles) = markersIds
+	genoPED    = cbind (0, samplesIds, 0,0,0,-9, talleles)
+
 	filenamePED = paste0 (plinkFilename, "-plink.ped")
 	write.table (file=filenamePED, genoPED, col.names=F, row.names=F, quote=F, sep="\t")
+
 }
 
 #----------------------------------------------------------
@@ -73,8 +118,8 @@ tabAlleles <- function (alleles) {
 	ncols = ncol (alleles)
 	nrows = nrow (alleles)
 	alleles = t (alleles)
-	tabs <- function (x) {return (sprintf("%s\t%s", substr(x,1,1),substr(x,2,2)))}
-	allelesTabbed =matrix (sapply (alleles,tabs), ncol=nrows, nrow=ncols, byrow=F)
+	tabs <- function (x) {return (sprintf("%s %s", substr(x,2,2),substr(x,1,1)))}
+	allelesTabbed =matrix (mclapply (alleles,tabs,mc.cores=4), ncol=nrows, nrow=ncols, byrow=F)
 	return (allelesTabbed)
 }
 
@@ -91,18 +136,20 @@ structureFile         = args [3]
 
 plinkFilename = strsplit (gwaspolyGenotypeFile, split="[.]")[[1]][1]
 
+# Read the genotype
 gwaspolyGenotype = read.table (file=gwaspolyGenotypeFile, header=T)
-gw = gwaspolyGenotype
-
-# Write PED
-createFilePED (gwaspolyGenotype, plinkFilename)
-# Write MAP
-createFileMAP (gwaspolyGenotype, plinkFilename)
+genotype = gwaspolyGenotype [order (gwaspolyGenotype$Chrom,	gwaspolyGenotype$Position),]
 
 ## Write and format tassel phenotype
 createPlinkPhenotype (gwaspolyPhenotypeFile)
 
+# Write MAP
+createFileMAP (genotype, plinkFilename)
+
+# Write PED
+createFilePED (genotype, plinkFilename)
+
 ## Write and format tassel structure
-#writeTasselStruct (structureFile) {
+writeTasselStruct (structureFile) {
 
 
